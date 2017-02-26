33fcb7f047e2f7f4d7d0b72975e75a5eae6c497c
Chris Etheridge <chris.etheridge@cognician.com>
2017-02-26 14:56:42 +0200

    0.1.1 Added `files-with` `files-of` `print->` and `pprint->`

--- src/utensil/core.clj
+++ src/utensil/core.clj
@@ -1,6 +1,51 @@
-(ns utensil.core)
+(ns utensil.core
+  (:require
+   [clojure.java.io :as io])
+  (:import
+   [java.io File]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(def all-files #(file-seq (io/file %)))
+
+
+(defn files-with
+  "Returns a seq of files that have content that match the
+   regex provided.
+
+   Example:
+   `(files-with \"/resources/templates/\" #\"application/js\")`"
+  [path re]
+  (when-let [fs (all-files path)]
+    (for [f fs
+          :when (.isFile f)
+          :let [contents (slurp f)]
+          :when (re-find re contents)]
+      f)))
+
+
+(defn files-of
+  "Returns a seq of files that match the given pred.
+   Predicate must take file as the first argument.
+
+   Example:
+   `(files-of \"/resources/public/css/\" #(re-find #\".scss\" (.getName %))`"
+  [path pred]
+  (when-let [fs (all-files path)]
+    (for [f fs
+          :when (pred f)]
+      f)))
+
+
+(defn print->
+  "Variant of `print` that returns the args after printing."
+  [& args]
+  (prn args)
+  args)
+
+
+(defn pprint->
+  "Variant of `clojure.pprint/pprint` that returns the args
+   after pprinting."
+  [& args]
+  (clojure.pprint/pprint args)
+  args)
